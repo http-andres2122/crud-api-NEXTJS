0f5004013aaa683c4e5bfe4b7e811e3ab4c38a2c
Andres <19674617+http-andres2122@users.noreply.github.com>
2023-11-02 22:23:12 -0500

    v1.0

---
+++ app/api/blogs/[id]/route.ts
@@ -0,0 +1,14 @@
+export const GET = async (req: Request) => {
+  console.log("GET Id");
+  //get a post by id
+};
+
+export const PUT = async (req: Request) => {
+  console.log("PUT Id");
+  //update a post by id
+};
+
+export const DELETE = async (req: Request) => {
+  console.log("DELETE Id");
+  //delete a post by id
+};
